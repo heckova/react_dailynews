39363303b10eada6429b45853d2c6d13f1671fa9
Alta <heckova.l@gmail.com>
2018-01-11 14:24:10 +0000

    created Route for Home, created Header an Footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,24 @@
 import React, {Component} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+//COMPONENTS
+import Header from "./header";
+import Footer from "./footer";
+//CONTAINERS
+import Home from "../containers/home";
 
 class App extends Component {
 
     render() {
         return (
-            <div>Hello World</div>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                    </Switch>
+                    <Footer/>
+                </div>
+            </BrowserRouter>
         )
     }
 }
