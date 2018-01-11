861f659680517d8ce6b493fdc1aac80efd79503a
Alta <heckova.l@gmail.com>
2018-01-11 15:56:53 +0000

    imported redux settings, created functions mapStateToProps and mapDispatchToProps, changed export

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,12 +1,30 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {latestNews} from "../actions";
+import {bindActionCreators} from "redux";
+
+//COMPONENTS
+import LatestNews from "../components/home/latest";
 
 class Home extends Component {
 
     render() {
         return (
-            <div>Home</div>
+            <div>
+                <LatestNews/>
+            </div>
         )
     }
 }
 
-export default Home;
+function mapStateToProps(state) {
+    return {
+        articles: state.articles
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({latestNews},dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
