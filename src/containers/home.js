d905bfa573339c243f44222fb8a6ef3a61302dcf
Alta <heckova.l@gmail.com>
2018-01-12 16:07:18 +0000

    added OtherNews component

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,21 +1,24 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {latestNews} from "../actions";
+import {latestNews, otherNews} from "../actions";
 import {bindActionCreators} from "redux";
 
 //COMPONENTS
 import LatestNews from "../components/home/latest";
+import OtherNews from "../components/home/otherNews";
 
 class Home extends Component {
 
     componentDidMount() {
         this.props.latestNews();
+        this.props.otherNews();
     }
 
     render() {
         return (
             <div>
                 <LatestNews latest={this.props.articles.latest}/>
+                <OtherNews otherNews={this.props.articles.other}/>
             </div>
         )
     }
@@ -28,7 +31,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({latestNews}, dispatch)
+    return bindActionCreators({latestNews, otherNews}, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
