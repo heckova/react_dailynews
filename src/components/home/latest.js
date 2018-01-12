83430a416ea85899be68d05c82f63141f432f47f
Alta <heckova.l@gmail.com>
2018-01-12 15:36:32 +0000

    written latest component

--- src/components/home/latest.js
+++ src/components/home/latest.js
@@ -1,10 +1,31 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+const showLatest = ({latest}) => {
+    if (latest) {
+        return latest.map((item) => {
+            return (
+                <Link to={`/news/${item.id}`} key={item.id} className="item">
+                    <div
+                        className="image_cover"
+                        style={{background:`url(/images/articles/${item.img})`}}
+                        >
+                        <div className="description">
+                            <span>{item.category}</span>
+                            <div>{item.title}</div>
+                        </div>
+                    </div>
+                </Link>
+            )
+        })
+    }
+};
 
 const LatestNews = (props) => {
     console.log(props);
     return (
-        <div>
-            LATEST NEWS 2
+        <div className="home-latest">
+            {showLatest(props)}
         </div>
     )
 };
