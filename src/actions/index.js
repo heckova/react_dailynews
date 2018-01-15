7c4bbcdb990e02699b65c90b12a6847234731687
Alta <heckova.l@gmail.com>
2018-01-15 13:43:15 +0000

    added action for latestGallery

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,3 +19,13 @@ export function otherNews() {
         payload: request
     }
 }
+
+export function latestGallery() {
+    const request = fetch(`${URL}/galleries?_order=desc&_limit=2`, {method: "GET"})
+        .then(response => response.json());
+
+    return {
+        type: "GET_LATEST_GALLERY",
+        payload: request
+    }
+}
