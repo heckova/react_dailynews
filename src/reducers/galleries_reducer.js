3aff1d677044f85b754ee49461aaf23e2534db3a
Alta <heckova.l@gmail.com>
2018-01-17 11:02:26 +0000

    added reducers for get selected gallery and clear selected gallery

--- src/reducers/galleries_reducer.js
+++ src/reducers/galleries_reducer.js
@@ -1,7 +1,11 @@
-export default function (state={},action) {
-    switch(action.type){
+export default function (state = {}, action) {
+    switch (action.type) {
         case "GET_LATEST_GALLERY":
-            return {...state, latestGallery:action.payload};
+            return {...state, latestGallery: action.payload};
+        case "GET_SELECTED_GALLERY":
+            return {...state, selected: action.payload};
+        case "CLEAR_SELECTED_GALLERY":
+            return {...state, selected: action.payload};
         default:
             return state
     }
